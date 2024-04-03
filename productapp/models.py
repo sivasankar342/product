d9601c61fae83d0efe337abb940b87f8d96f0d4f
sivasankar342 <sivasankar@rekonsys.tech>
2024-04-03 10:11:10 +0530

    sivasankar

---
+++ productapp/models.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    USER_ROLES = (
+        ('user','User'),
+        ('dealer','Dealer'),
+       
+       
+    )
+   
+    user=models.BooleanField(default=False)
+    dealer=models.BooleanField(default=False)
+
+
+
+class product(models.Model):
+   # id=models.IntegerField(primary_key=True)
+    pname=models.CharField(max_length=100)
+    qty=models.IntegerField(default=1)
+    price=models.DecimalField(max_digits=10,decimal_places=2)
+    img=models.URLField(max_length=100)
+    parent=models.ForeignKey('self',on_delete=models.CASCADE,null=True,blank=True)
